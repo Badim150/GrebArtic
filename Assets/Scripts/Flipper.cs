79524e5eb8ac3048e6bb6c1d08b628c72294cbbe
Badim150 <cm_margarido@hotmail.com>
2017-06-22 14:39:34 +0100

    Init

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+    public float restPosition = 0;
+    public float pressedPosition = 45;
+    public float hitStrenght = 10000;
+    public float flipperDamper = 150;
+
+    HingeJoint hinge;
+
+    public string inputName;
+
+	void Start ()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+	}
+	
+
+	void Update ()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamper;
+
+        if (Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+
+	}
+}
---
+++ Assets/Scripts/Plunger.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plunger : MonoBehaviour {
+
+    float power;
+    float minPower = 0;
+    bool ballReady;
+    List<Rigidbody> ballList;
+
+    public float maxPower = 100;
+    public Slider powerSlider;
+
+    private void Start()
+    {
+        powerSlider.minValue = 0;
+        powerSlider.maxValue = maxPower;
+        ballList = new List<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        if (ballReady)
+        {
+            powerSlider.gameObject.SetActive(true);
+        }
+        else
+        {
+            powerSlider.gameObject.SetActive(false);
+        }
+
+
+
+        powerSlider.value = power;
+        if (ballList.Count > 0)
+        {
+            ballReady = true;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (power <= maxPower)
+                {
+                    power += 50 * Time.deltaTime;
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                foreach (Rigidbody r in ballList)
+                {
+                    r.AddForce(power*Vector3.forward);
+                }
+            }
+        }
+        else
+        {
+            ballReady = false;
+            power = minPower;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ballList.Add(other.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ballList.Remove(other.GetComponent<Rigidbody>());
+            power = minPower;
+        }
+    }
+
+}
